abbd1f747879cce53cb09e167419218167b82307
duanyimiao <duanyimiao@wepiao.com>
2016-01-21 11:06:28 +0800

    first-commit

---
+++ src/test/java/com/movie/rec/test/service/BaseTest.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:movie-recommender
+ * File Name:BaseTest.java
+ * Package Name:com.movie.rec.test.service
+ * Date:2016年1月11日上午10:21:23
+ *
+*/
+
+package com.movie.rec.test.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * ClassName:BaseTest <br/>
+ * Date:     2016年1月11日 上午10:21:23 <br/>
+ * @author   dynamo
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:config-bootstrap.xml"})
+public class BaseTest {
+  //test git
+}
